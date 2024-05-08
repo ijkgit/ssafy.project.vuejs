7c5f65cdc3a34a8e1e845203d6051062db84d73d
jay <dlaworud@koreatech.ac.kr>
2024-05-08 17:48:23 +0900

    feat: 메인 페이지 컨버팅 진행 중

---
+++ src/components/common/HeaderMain.vue
@@ -0,0 +1,110 @@
+<script setup></script>
+
+<template>
+  <!-- <div>Header</div> -->
+  <!-- 상단 nav bar -->
+  <div id="nav" class="navbar-expand-lg fixed-top border-bottom">
+    <div class="container">
+      <header class="d-flex align-items-center justify-content-around py-3">
+        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+          <h1 id="header">나들이</h1>
+        </a>
+        <button
+          class="navbar-toggler ms-2"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#nav-menu"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="white"
+            class="bi bi-list"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
+            />
+          </svg>
+        </button>
+
+        <div id="nav-menu" class="collapse navbar-collapse justify-content-end">
+          <ul class="nav col-12 col-md-auto mb-2 mb-md-0">
+            <li><a id="nav-list" href="#" class="nav-link px-2">핫플 자랑하기</a></li>
+            <li>
+              <a
+                id="nav-list"
+                href="#"
+                class="nav-link px-2"
+                onclick="javascript:goMyTourismPlanList();"
+                >나의 여행 계획</a
+              >
+            </li>
+            <li>
+              <a id="nav-list" href="#" class="nav-link px-2" onclick="javascript:goLocalTourism();"
+                >지역별 여행지</a
+              >
+            </li>
+            <li>
+              <a
+                id="nav-list"
+                href="#"
+                class="nav-link px-2"
+                onclick="javascript:goShareTourismInfo();"
+                >여행 정보 공유</a
+              >
+            </li>
+          </ul>
+          <c:if test="${empty login}">
+            <form id="signInFail" class="ms-5">
+              <button
+                id="primary-outline-button"
+                type="button"
+                class="btn btn-outline-primary me-2"
+                onclick="javascript:goSignUp()"
+              >
+                회원가입
+              </button>
+              <button
+                id="primary-button"
+                type="button"
+                class="btn btn-primary"
+                onclick="javascript:goSignIn();"
+              >
+                로그인
+              </button>
+            </form>
+          </c:if>
+
+          <c:if test="${not empty login}">
+            <form id="signInSuccess" class="col-md-3 text-end">
+              <button
+                id="primary-outline-button"
+                type="button"
+                class="btn btn-outline-primary me-2"
+                onclick="javascript:goMyPage();"
+              >
+                마이페이지
+              </button>
+              <button
+                id="primary-button"
+                type="button"
+                class="btn btn-primary"
+                onclick="javascript:goSignOut();"
+              >
+                로그아웃
+              </button>
+            </form>
+          </c:if>
+        </div>
+      </header>
+    </div>
+  </div>
+  <RouterView />
+</template>
+
+<style scoped>
+@import "@/assets/main/main.css";
+</style>
