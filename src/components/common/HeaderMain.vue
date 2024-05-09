b1fcd187941cb9518196a7704c23ee560daebc14
jay <dlaworud@koreatech.ac.kr>
2024-05-09 16:38:57 +0900

    feat: header 적용

--- src/components/common/HeaderMain.vue
+++ src/components/common/HeaderMain.vue
@@ -1,20 +1,20 @@
 <script setup>
-import Header1 from '@/components/common/Header1.vue'
-import Header2 from '@/components/common/Header2.vue'
-import { useRoute } from 'vue-router'
+import Header1 from "@/components/common/Header1.vue";
+import Header2 from "@/components/common/Header2.vue";
+import { useRoute } from "vue-router";
 
 const route = useRoute();
-console.log(route)
+console.log(route);
 </script>
 
 <template>
   <!-- <div>Header</div> -->
   <!-- 상단 nav bar -->
-  <Header1 v-if="route.name === 'main'"/>
-  <Header2 v-else/>
+  <Header1 v-if="route.name === 'main'" />
+  <Header2 v-else />
   <RouterView />
 </template>
 
 <style scoped>
-@import "@/assets/main/main.css";
+/* @import "@/assets/main/main.css"; */
 </style>
